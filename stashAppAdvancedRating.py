e307dd81a8d090e7f57544fa063ef98184c94063
Jens Ackou <shackofnoreturn@gmail.com>
2025-06-17 00:52:55 +0200

    Initial test

---
+++ stashAppAdvancedRating.py
@@ -0,0 +1,31 @@
+import re
+
+# Predefined categories
+CATEGORIES = ["acting", "camera", "story", "intensity", "chemistry"]
+TAG_PATTERN = re.compile(r"^([a-z_]+)_(\d)$")
+
+def main(stash):
+    scenes = stash.findScenes({})
+    for scene in scenes:
+        tags = [tag['name'] for tag in scene['tags']]
+        scores = {}
+
+        for tag in tags:
+            match = TAG_PATTERN.match(tag)
+            if match:
+                category, score = match.groups()
+                if category in CATEGORIES:
+                    scores[category] = int(score)
+
+        if scores:
+            average = sum(scores.get(cat, 0) for cat in CATEGORIES) / len(CATEGORIES)
+            final_rating = round(average)
+        else:
+            final_rating = 0
+
+        if scene.get("rating") != final_rating:
+            print(f"Updating scene '{scene['title']}' to rating {final_rating}")
+            # stash.updateScene({
+            #     "id": scene['id'],
+            #     "rating": final_rating
+            # })
