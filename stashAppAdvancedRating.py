731e8b67f342118c5c048683102b9b9b721ed86c
Jens Ackou <shackofnoreturn@gmail.com>
2025-06-17 04:00:08 +0200

    User adjustable categories

--- stashAppAdvancedRating.py
+++ stashAppAdvancedRating.py
@@ -1,57 +1,40 @@
 import re
+from stashapi.stashapp import StashInterface
 
-try:
-    import stashapi.log as log
-    from stashapi.stashapp import StashInterface
-
-except ModuleNotFoundError:
-    print(
-        "You need to install the stashapi module. (pip install stashapp-tools)",
-        file=sys.stderr,
-    )
-    exit
-
-FAKTORCONV = 6.25
-FRAGMENT = json.loads(sys.stdin.read())
-# MODE = FRAGMENT['args']['mode']
-MODE = FRAGMENT["args"].get("mode")
-PLUGIN_DIR = FRAGMENT["server_connection"]["PluginDir"]
-stash = StashInterface(FRAGMENT["server_connection"])
-
-# Predefined categories
-CATEGORIES = ["acting", "camera", "story", "intensity", "chemistry"]
 TAG_PATTERN = re.compile(r"^([a-z_]+)_(\d)$")
 
-def main(stash):
-    log.info(f"Plugin Dir {PLUGIN_DIR} ")
-    scenes = stash.findScenes({})
-    for scene in scenes:
-        tags = [tag['name'] for tag in scene['tags']]
-        scores = {}
+class StashRatingPlugin:
+    def __init__(self, stash: StashInterface):
+        self.stash = stash
 
-        for tag in tags:
-            match = TAG_PATTERN.match(tag)
-            if match:
-                category, score = match.groups()
-                if category in CATEGORIES:
-                    scores[category] = int(score)
+    def get_configured_categories(self):
+        category_str = self.stash.settings.get('categories', 'acting,camera,story,intensity,chemistry')
+        return [cat.strip().lower() for cat in category_str.split(',') if cat.strip()]
 
-        if scores:
-            average = sum(scores.get(cat, 0) for cat in CATEGORIES) / len(CATEGORIES)
-            final_rating = round(average)
-        else:
-            final_rating = 0
+    def scan_and_rate_scenes(self):
+        categories = self.get_configured_categories()
+        scenes = self.stash.find_scenes({})
 
-        if scene.get("rating") != final_rating:
-            print(f"Updating scene '{scene['title']}' to rating {final_rating}")
-            # stash.updateScene({
-            #     "id": scene['id'],
-            #     "rating": final_rating
-            # })
+        for scene in scenes:
+            tags = [tag['name'].lower() for tag in scene['tags']]
+            scores = {}
 
+            for tag in tags:
+                match = TAG_PATTERN.match(tag)
+                if match:
+                    category, score = match.groups()
+                    if category in categories:
+                        scores[category] = int(score)
 
-def do_logcat(args):
-    print(f"TESTING PLUGIN TASKS")
+            if scores:
+                average = sum(scores.get(cat, 0) for cat in categories) / len(categories)
+                final_rating = round(average)
+            else:
+                final_rating = 0
 
-def do_crash_report(args):
-    log.info(f"TESTING PLUGIN 2")
+            if scene.get("rating") != final_rating:
+                print(f"Updating scene '{scene['title']}' to rating {final_rating}")
+                self.stash.update_scene(
+                    scene_id=scene['id'],
+                    rating=final_rating
+                )
